825575c3595b36567a746cf3bfbdb52d62bc3921
matteolebrave <Matteo.Lebrave@bordeaux-inp.fr>
2026-08-31 17:41:34 +0200

    add map.py with map display and planes icons

--- UI.py
+++ UI.py
@@ -1,13 +1,15 @@
 from tkinter import * 
+from map import displayMap
+
 
 def scan():
-    lat = latitude.get()
-    lon = longitude.get()
+    LAT = latitude.get()
+    LON = longitude.get()
     radius = rayon.get()
 
 
-    print("Latitude :", lat)
-    print("Longitude :", lon)
+    print("Latitude :", LAT)
+    print("Longitude :", LON)
     print("Rayon :", radius)
 
     AVIONS = [{
@@ -34,6 +36,9 @@ def scan():
         Label(RESULTS, text=AVIONS[i]["callsign"]).grid(row=i+2, column=0, padx=10, pady=10)
         Label(RESULTS, text=AVIONS[i]["callsign"]).grid(row=i+2, column=1, padx=10, pady=10)
         Label(RESULTS, text=AVIONS[i]["geo_altitude"]).grid(row=i+2, column=2, padx=10, pady=10)
+    
+    Button(MAIN,text="Afficher sur la Carte !", command=lambda: displayMap(LAT,LON,AVIONS)).grid(row=2, column=1)
+
 
 ## window declaration
 root = Tk()
@@ -41,10 +46,11 @@ root.title("air-scanner -- Leop & Mat")
 root.geometry("800x600")
 
 
-## first pannel ✈ AIR SCANNER          │
+## first pannel
+#│           ✈ AIR SCANNER             │
 #│                                     │
-#│ Latitude   [48.8566]                │
-#│ Longitude  [2.3522]                 │
+#│ latitude   [48.8566]                │
+#│ longitude  [2.3522]                 │
 #│ Rayon      [50] km                  │
 
 MAIN = Frame(root, borderwidth=2, relief=GROOVE)
@@ -61,9 +67,9 @@ INPUT = Frame(MAIN, borderwidth=2, relief=GROOVE)
 INPUT.grid(row=1, column=0, padx=10, pady=10)
 
 
-Label(INPUT, text="Latitude : ").grid(row=1, column=0, padx=10, pady=10)
+Label(INPUT, text="latitude : ").grid(row=1, column=0, padx=10, pady=10)
 Entry(INPUT, textvariable=latitude, width=30).grid(row=1, column=1, padx=10, pady=10)
-Label(INPUT, text="Longitude : ").grid(row=2, column=0, padx=10, pady=10)
+Label(INPUT, text="longitude : ").grid(row=2, column=0, padx=10, pady=10)
 Entry(INPUT, textvariable=longitude, width=30).grid(row=2, column=1, padx=10, pady=10)
 Label(INPUT, text="Rayon : ").grid(row=3, column=0, padx=10, pady=10)
 Entry(INPUT, textvariable=rayon, width=30).grid(row=3, column=1, padx=10, pady=10)
@@ -74,6 +80,7 @@ Button(INPUT,text="SCANNER", command=scan).grid(row=4, column=1)
 
 RESULTS = Frame(MAIN, borderwidth=2, relief=GROOVE)
 RESULTS.grid(row=2, column=0, padx=10, pady=10)
+
 
     
 
