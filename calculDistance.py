4e53bde4880c1bf90e44af35e008a0b724062fd1
matteolebrave <Matteo.Lebrave@bordeaux-inp.fr>
2026-08-31 15:44:44 +0200

    add paramaters.py

--- calculDistance.py
+++ calculDistance.py
@@ -1,12 +1,9 @@
 from havershine import havershine
-
-LATITUDE = 48.870339998449
-LONGITUDE = 2.315951028055
-
-avion = {
-    "latitude": 48.868901999141,
-    "longitude": 2.314898009559
-}
+from parameters import RADIUS, LATITUDE ,LONGITUDE, AVION
+import pytest
 
 
-print(havershine(LATITUDE, LONGITUDE, avion))
+def test_distance():
+    d = havershine(LATITUDE,LONGITUDE,AVION)
+    
+    assert d <= RADIUS*10**3
