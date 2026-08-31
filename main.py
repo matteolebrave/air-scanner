7e282ffa2dedc0253c43e5d908b5765417c69b72
Leopold Vichet <leopold.vichet@gmail.com>
2026-08-31 17:26:07 +0200

    Création JSON et ajout distances au JSON

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from position import calculate_bounding_box
 from apiRequests import get_aircraft
 from havershine import havershine
@@ -25,12 +27,14 @@ for avion in aircraft:
     distance = havershine(latitude, longitude, avion)
 
     if distance <= radius * 1000:
-        aircraft_in_radius.append((avion, distance))
+        avion["distance"] = distance / 1000
+        aircraft_in_radius.append((avion))
 
 print(f"{len(aircraft_in_radius)} aéronefs dans le rayon de {radius} km")
 
-for avion, distance in aircraft_in_radius:
-    print(avion["callsign"], round(distance / 1000, 2), "km")
+#for avion, distance in aircraft_in_radius:
+#    print(avion["callsign"], round(distance / 1000, 2), "km")
 
+json_result = json.dumps(aircraft_in_radius, indent=4)
 
-
+print(json_result)
