1034a38f2bb4b3848a8b1b766b97aa6025b37321
matteolebrave <Matteo.Lebrave@bordeaux-inp.fr>
2026-08-31 13:30:16 +0200

    apiRequests format json

--- apiRequests.py
+++ apiRequests.py
@@ -1,30 +1,43 @@
 import requests
 import json
 
-apiKEY = "286bc0ebbed2d9942a0ce82e727b4750" # api key
-
-realTime = "https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226" # querry for real time flight
-
-flightDate ="&flight_date="
-
-nbFlight = "&limit=" # nb of flight
-
-offset = "&offset=" #redondance
+querry = "https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226" # querry for real time flight
 
 
-url = realTime #+ apiKEY + nbFlight + "100" + offset + "0"
-
-#print(url)
-
-apiResults = requests.get(url)
+apiResults = requests.get(querry)
 apiResponse = apiResults.json()
-print(apiResponse)
-#print(apiResponse)
 
+data_list = []
+states = apiResponse["states"]
+
+for data in states:
+
+    text ={ 
+    "icao24": data[0],
+    "callsign": data[1],
+    "origin_country": data[2],
+    "time_position": data[3],
+    "last_contact": data[4],
+    "longitude": data[5],
+    "latitude": data[6],
+    "baro_altitude": data[7],
+    "on_ground": data[8],
+    "velocity": data[9],
+    "true_track": data[10],
+    "vertical_rate": data[11],
+    "sensors": data[12],
+    "geo_altitude": data[13],
+    "squawk": data[14],
+    "spi": data[15],
+    "position_source": data[16],
+    #"category": data[17]
+    }
+
+    data_list.append(text)
+    
 with open("apiResponse.json", "w") as f:
-    states = apiResponse["states"]
-    print(states)
-    f.write(json.dumps(apiResponse, indent=4))
+    f.write(json.dumps(data_list, indent = 4))
+
 
 # with open("apiResponse.json" ,"r") as f:
 #     f.read()
