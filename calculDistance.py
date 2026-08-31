969ef360a0f855b2d3b5b660b5207ae343ee3091
matteolebrave <Matteo.Lebrave@bordeaux-inp.fr>
2026-08-31 14:09:49 +0200

    distance entre deux points + havershine

---
+++ calculDistance.py
@@ -0,0 +1,12 @@
+from havershine import havershine
+
+LATITUDE = 48.870339998449
+LONGITUDE = 2.315951028055
+
+avion = {
+    "latitude": 48.868901999141,
+    "longitude": 2.314898009559
+}
+
+
+print(havershine(LATITUDE, LONGITUDE, avion))
